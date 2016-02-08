f61d002f6b13c23f6c294dd09092291346675ed4
Mel Huang <hello@melaniehuang.com>
2016-02-08 11:53:37 +1100

    Using SVG rather than div

--- visualisation.js
+++ visualisation.js
@@ -3,6 +3,9 @@ var critics = [82, 98, 88, 93, 91, 97, 96, 96, 92, 72, 98, 92, 90, 99, 80, 94, 9
 
 var dataset = [];
 
+var width = 1000,
+    barHeight = 20;
+
 for (var i = 0; i < ratings.length; i++) {
   var r = ratings[i];
   var c = critics[i];
@@ -13,16 +16,23 @@ for (var i = 0; i < ratings.length; i++) {
 //mapping values to a px width
 var x = d3.scale.linear()
     .domain([0, d3.max(dataset)])
-    .range([0, 1000]);
+    .range([0, width]);
 
-//Select the chart container using class selector
-d3.select(".chart")
-//Initiate the data join by defining the selection to wich we will join data - in this case the divs
-  .selectAll("div")
-//Joining the data to the selection
+var chart = d3.select(".chart")
+    .attr("width", width)
+    .attr("height", barHeight * dataset.length);
+
+var bar = chart.selectAll("g")
     .data(dataset)
-  .enter().append("div")
-//Set the width of each new bar as a multiple of the set ratio
-    .style("width", function(d) { return x(d) + "px"; })
- //Add text labels
+  .enter().append("g")
+    .attr("transform", function(d, i) { return "translate(0," + i * barHeight + ")"; });
+
+bar.append("rect")
+    .attr("width", x)
+    .attr("height", barHeight - 1);
+
+bar.append("text")
+    .attr("x", function(d) { return x(d) - 3; })
+    .attr("y", barHeight / 2)
+    .attr("dy", ".35em")
     .text(function(d) { return d; });
