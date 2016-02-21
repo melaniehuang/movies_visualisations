4ea360da487f974031f7a310013cf9cb20eb07f1
Mel Huang <hello@melaniehuang.com>
2016-02-21 17:40:00 +1100

    getting csv hooked up to bars

--- visualisation.js
+++ visualisation.js
@@ -1,21 +1,40 @@
 // var x;
 var oCSV;
+var barWidth = 1000;
+var barHeight = 32;
+var gap = 4;
+//colors
+var oscarsGold = "#b7a261"
 
 function preload() {
   oCSV = loadTable("data/oscars.csv", "csv", "header");
+  print("Loaded CSV!");
+  numRows = oCSV.getRowCount();
 }
 
 function setup() {
-  createCanvas(windowWidth,windowHeight);
   // x = createGraphics(500,500);
-
-  print(oCSV.getRowCount() + " total rows in table");
-  print(oCSV.getColumnCount() + " total columns in table");
-  print(oCSV.getColumn("Movie"));
+  var totalHeight = (barHeight * oCSV.getRowCount()) + (gap * oCSV.getRowCount());
+  createCanvas(windowWidth, totalHeight);
+  noLoop();
 }
 
 function draw(){
-  background(100);
-  
+  background("#eee");
+  fill(oscarsGold);
+  noStroke();
+  for (var i = 0; i < oCSV.getRowCount(); i++) {
+  	var movie = oCSV.getRow(i);
+    var title = movie.getString(1);
+    var user = movie.getNum(6);
+
+    rect((windowWidth/2)-(barWidth/2),(barHeight*i)+(gap*i), user*barWidth, barHeight);
+    
+    fill(255);
+    textSize(14);
+    textAlign(LEFT);
+    text(title, 20 + (windowWidth/2)-((barWidth/2)), (barHeight*i)+(gap*i)+22);
+    fill(oscarsGold);
+  };
 
 }
