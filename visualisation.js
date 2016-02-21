a443cff03b2fe271cf915205b8cf87a735660c7a
Mel Huang <hello@melaniehuang.com>
2016-02-21 14:06:42 +1100

    Changing approach and doing simple proof of concept

--- visualisation.js
+++ visualisation.js
@@ -1,34 +1,21 @@
-var width = 1000,
-    barHeight = 20;
+// var x;
+var oCSV;
 
-var x = d3.scale.linear()
-    .range([0, width]);
+function preload() {
+  oCSV = loadTable("data/oscars.csv", "csv", "header");
+}
 
-var chart = d3.select(".chart")
-    .attr("width", width);
+function setup() {
+  createCanvas(windowWidth,windowHeight);
+  // x = createGraphics(500,500);
 
-d3.tsv("/data/oscars.tsv", type, function(error, data) {
-  x.domain([0, d3.max(data, function(d) { return d.value; })]);
+  print(oCSV.getRowCount() + " total rows in table");
+  print(oCSV.getColumnCount() + " total columns in table");
+  print(oCSV.getColumn("Movie"));
+}
 
-  chart.attr("height", barHeight * data.length);
+function draw(){
+  background(100);
+  
 
-  var bar = chart.selectAll("g")
-      .data(data)
-    .enter().append("g")
-      .attr("transform", function(d, i) { return "translate(0," + i * barHeight + ")"; });
-
-  bar.append("rect")
-      .attr("width", function(d) { return x(d.value); })
-      .attr("height", barHeight - 1);
-
-  bar.append("text")
-      .attr("x", function(d) { return x(d.value) - 3; })
-      .attr("y", barHeight / 2)
-      .attr("dy", ".35em")
-      .text(function(d) { return d.value; });
-});
-
-function type(d) {
-  d.value = +d.value; // coerce to number
-  return d;
 }
