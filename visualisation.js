0f6ccce095b55031f3040ab09537e9ad52640b74
Mel Huang <hello@melaniehuang.com>
2016-02-25 00:14:53 +1100

    Adding Strings to the visualisation and doing math to the budgets

--- visualisation.js
+++ visualisation.js
@@ -1,60 +1,67 @@
-// var x;
 var oCSV;
-var barWidth = 600;
-var barHeight = 120;
-var gap = 2;
-//colors
+var barWidth = 400;
+var barHeight = 60;
+
 var oscarsGold = "#b7a261"
 
 function preload() {
   oCSV = loadTable("data/oscars.csv", "csv", "header");
-  print("Loaded CSV!");
-  numRows = oCSV.getRowCount();
+  print("Loaded CSV!");  
 }
 
 function setup() {
   // x = createGraphics(500,500);
-  var totalHeight = (barHeight * oCSV.getRowCount()) + (gap * oCSV.getRowCount());
+  var totalHeight = barHeight * oCSV.getRowCount();
   createCanvas(windowWidth, totalHeight);
-  noLoop();
-  rectMode(CENTER);
+
+  background(241, 246, 25);
+  fill(0);
+
+  
+  for (var i = 0; i < oCSV.getRowCount(); i++) {   
+    var movie = oCSV.getRow(i);
+    var user = movie.getNum(6)*100;
+    var critic = movie.getNum(5)*100;
+    var ratingDiff =  critic - user;
+    
+    drawGrid(totalHeight, i);
+
+    var startingPoint = (windowWidth/100)*user;
+
+    noStroke();
+    rect(startingPoint, (barHeight*i), (windowWidth/100)*ratingDiff, barHeight);
+    
+    writeTitles(movie, i);
+    fill(0);
+  };
+}
+
+function drawGrid(wh, i){
+    stroke(0);
+    
+    //to check
+    // for (var w = 0; w < 10; w++){
+    //   line((windowWidth/10)*w, 0, (windowWidth/10)*w, wh);
+    // }
+
+    line(0, (barHeight*i), windowWidth, (barHeight*i));
+}
+
+function writeTitles(m, i){
+    textSize(12);
+    textAlign(LEFT); 
+    
+    var budget = m.getString(7)/1000000;
+    budget = Math.round(budget*10)/10;
+
+
+    if (m.getString(2) == "W"){
+      text(m.getString(1) + "  |  " + m.getString(0) + " Winner" + "  |  Won " + m.getString(3) + " / " + m.getString(4) + " oscars  |  $" + budget + "m budget  |  ROI " + m.getString(9) + "%" , 20, (barHeight*i)+(barHeight/2+5));
+    } else {
+      text(m.getString(1) + "  |  " + m.getString(0) + " Nominee" + "  |  Won " + m.getString(3) + " / " + m.getString(4) + " oscars  |  $" + budget + "m budget  |  ROI " + m.getString(9) + "%" , 20, (barHeight*i)+(barHeight/2+5));
+    }
 }
 
 function draw(){
-  background("#eee");
   
-  fill(200);
-  noStroke();
-  for (var i = 0; i < oCSV.getRowCount(); i++) {
-  	var movie = oCSV.getRow(i);
-    var title = movie.getString(1);
-    var critic = movie.getNum(5);
-
-    var winners = movie.getString(2);
-         
-    var ratingDiff =  critic - movie.getNum(6);
-    print(ratingDiff);
-
-    if (winners == "W"){
-    	fill(255,237,0);
-    }
-
-    rect((windowWidth/2),60+(barHeight*i)+(gap*i), critic*barWidth, barHeight);
-    
-    if (ratingDiff < -0.05){
-    	fill("#1abc9c");
-    } else if (ratingDiff > 0.05){
-    	fill("#e74c3c");
-    } else {
-    	fill(150);
-    }
-
-    rect((windowWidth/2),60+(barHeight*i)+(gap*i), ratingDiff*barWidth, barHeight);
-
-    fill(255);
-    textSize(22);
-    textAlign(CENTER);
-    text(title, windowWidth/2, (barHeight*i)+(gap*i)+(barHeight/2+6));
-    fill(200);
-  };
 }
