9b73bda54b2364895a3e63423275ed27029be5f5
Mel Huang <hello@melaniehuang.com>
2016-02-29 18:33:56 +1100

    animations and copy

--- oscars/visualisation.js
+++ oscars/visualisation.js
@@ -32,13 +32,48 @@ function setup() {
   hideButton.position(windowWidth-130,10);
   hideButton.mousePressed(hideAll);
   
-  revealAll();
+  //revealAll();
 
-  var titles = selectAll('p');
-  for (var p = 0; p < titles.length; p++){
-    titles[p].hide();
+    
+  for (var i = 0; i < oCSV.getRowCount(); i++) {   
+      
+    var movie = oCSV.getRow(i);
+
+    fill(0);
+    noStroke();
+
+    var budget = movie.getString(7)/1000000;
+    budget = Math.round(budget*10)/10;
+
+    var won = "";
+
+    if (movie.getString(2) == "W"){
+      won = "Winner";
+    } else if (movie.getString(2) == "NA") {
+      won = "Pending result...";
+    } else {
+      won = "Nominee";
+    }
+
+    var titleP = createP(movie.getString(1) + '  |  ' + movie.getString(0) + ' ' + won + '  |  Won ' + movie.getString(3) + ' / ' + movie.getString(4) + ' oscars  |  US$' + budget + 'm budget  |  ' + movie.getString(9) + '% return' , 20, (barHeight*i)+(barHeight/2+5));
+    titleP.id("title" + i);
+    titleP.class("bar animated fadeIn");
+    titleP.parent('myList');
+    //titleP.mouseOver(revealOne);
+
+    if (movie.getString(10) == "W"){
+      titleP.addClass('bold');
+    }
   }
 
+
+  //hide all as default
+  // var titles = selectAll('p', '#myList');
+  // for (var p = 0; p < titles.length; p++){
+  //   titles[p].hide();
+  // }
+
+  //render boxes
   for (var i = 0; i < oCSV.getRowCount(); i++) {   
     strokeWeight(1);
     var movie = oCSV.getRow(i);
@@ -51,47 +86,28 @@ function setup() {
 }
 
 function revealAll() {
-    var titles = selectAll('p');
+    var titles = selectAll('p', '#myList');
     
     for (var p = 0; p < titles.length; p++){
       titles[p].show();
     }
-    
-    for (var i = 0; i < oCSV.getRowCount(); i++) {   
-      
-      var movie = oCSV.getRow(i);
-
-      fill(0);
-      noStroke();
-
-      var budget = movie.getString(7)/1000000;
-      budget = Math.round(budget*10)/10;
-
-      var won = "";
-
-      if (movie.getString(2) == "W"){
-        won = "Winner";
-      } else if (movie.getString(2) == "NA") {
-        won = "Pending result...";
-      } else {
-        won = "Nominee";
-      }
-
-      var titleP = createP(movie.getString(1) + '  |  ' + movie.getString(0) + ' ' + won + '  |  Won ' + movie.getString(3) + ' / ' + movie.getString(4) + ' oscars  |  $' + budget + 'm budget  |  ' + movie.getString(9) + '% return' , 20, (barHeight*i)+(barHeight/2+5));
-      titleP.id("title" + i);
-      titleP.class("bar");
-      titleP.parent('myList');
-
-      if (movie.getString(2) == "W"){
-        titleP.addClass('bold');
-      }
-
-      //text(movie.getString(1) + "  |  " + movie.getString(0) + " " + won + "  |  Won " + movie.getString(3) + " / " + movie.getString(4) + " oscars  |  $" + budget + "m budget  |  " + movie.getString(9) + "% return" , 20, (barHeight*i)+(barHeight/2+5));     
-    }
 }
 
+// function revealOne(){
+//   var revealIt = selectAll('p', '#myList');
+
+//   for (var p = 0; p < revealIt.length; p++){
+//     revealIt[p].hide();
+//   }
+  
+//   var i = round(mouseY/barHeight);
+//   console.log(i);
+  
+//   revealIt[1].show();
+// }
+
 function hideAll() {
-  var hideTitles = selectAll('p');
+  var hideTitles = selectAll('p', '#myList');
 
   for (var p = 0; p < hideTitles.length; p++){
     hideTitles[p].hide();
@@ -105,7 +121,7 @@ function renderRatings(m, i){
   var ratingDiff =  critic - user;
   var circle = false;
 
-  if (ratingDiff < -9 || ratingDiff > 9){
+  if (ratingDiff < -5 || ratingDiff > 5){
     fill(0);
     var circle = true;
   } else {
@@ -123,19 +139,27 @@ function renderRatings(m, i){
   if ((circle == true) && (ratingDiff < 0)) {
     strokeWeight(3);
     stroke(0);
-    fill(255,0,0);
+    fill(0,255,0);
     ellipse(startingPoint,(barHeight*i)+(barHeight/2),12,12);
+
+    noStroke();
+    fill(0);
+    text(user+"%", startingPoint +10, (barHeight*i)+(barHeight/2)+4);
   } else if ((circle == true) && (ratingDiff > 0)) {
     strokeWeight(3);
     stroke(0);
-    fill(0,255,0);
+    fill(255,0,0);
     ellipse(startingPoint,(barHeight*i)+(barHeight/2),12,12);
+
+    noStroke();
+    fill(0);
+    text(user+"%", startingPoint - 34, (barHeight*i)+(barHeight/2)+4);
   }
 
   strokeWeight(1);
 }
 
 //var mousePos = round(map(mouseY, 0, 40, -0.5, 0.5));
-
+//titleP.mouseOver(revealOne);
 
 
