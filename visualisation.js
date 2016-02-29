e751af2d604311f18a02e66bb3f27d24cb9525aa
Mel Huang <hello@melaniehuang.com>
2016-02-29 15:01:28 +1100

    Rearranging file to show/hide various html elements

--- visualisation.js
+++ visualisation.js
@@ -2,6 +2,10 @@ var oCSV;
 var barWidth = 400;
 var barHeight = 40;
 
+var canvas;
+var showButton;
+var hideButton;
+
 var oscarsGold = "#b7a261"
 
 function preload() {
@@ -10,61 +14,86 @@ function preload() {
 }
 
 function setup() {
-  // x = createGraphics(500,500);
   var totalHeight = barHeight * oCSV.getRowCount();
-  createCanvas(windowWidth, totalHeight);
-
+  canvas = createCanvas(windowWidth, totalHeight);
+  canvas.parent('myCanvas');
+  //noCanvas();
+  
   background(241, 246, 25);
   fill(0);
   noStroke();
+
+  showButton = createButton('Show all');
+  showButton.position(windowWidth-70,10);
+  showButton.mousePressed(revealAll);
+
+  hideButton = createButton('Hide all');
+  hideButton.position(windowWidth-130,10);
+  hideButton.mousePressed(hideAll);
   
+  revealAll();
+  var titles = selectAll('p');
+  titles.hide();
+
   for (var i = 0; i < oCSV.getRowCount(); i++) {   
     strokeWeight(1);
     var movie = oCSV.getRow(i);
-    
+      
     stroke(0);
     line(0, (barHeight*i), windowWidth, (barHeight*i));
     noStroke();
-    
-    writeTitles(movie, i);
     renderRatings(movie, i);
-
-    fill(0);
   }
 }
 
-function writeTitles(m, i){
-  textSize(12);
-  textAlign(LEFT); 
+function revealAll() {
+    //selectAll('p').show();
     
-  var budget = m.getString(7)/1000000;
-  budget = Math.round(budget*10)/10;
+    for (var i = 0; i < oCSV.getRowCount(); i++) {   
+      
+      var movie = oCSV.getRow(i);
 
-  var won = "";
+      fill(0);
+      noStroke();
 
-  if (m.getString(2) == "W"){
-    textStyle(BOLD);
-    won = "Winner";
-  } else if (m.getString(2) == "NA") {
-    textStyle(NORMAL);
-    won = "Pending result...";
-  } else {
-    textStyle(NORMAL);
-    won = "Nominee";
-  }
+      var budget = movie.getString(7)/1000000;
+      budget = Math.round(budget*10)/10;
 
-  text(m.getString(1) + "  |  " + m.getString(0) + " " + won + "  |  Won " + m.getString(3) + " / " + m.getString(4) + " oscars  |  $" + budget + "m budget  |  " + m.getString(9) + "% return" , 20, (barHeight*i)+(barHeight/2+5));
+      var won = "";
+
+      if (movie.getString(2) == "W"){
+        won = "Winner";
+      } else if (movie.getString(2) == "NA") {
+        won = "Pending result...";
+      } else {
+        won = "Nominee";
+      }
+
+      var titleP = createP(movie.getString(1) + '  |  ' + movie.getString(0) + ' ' + won + '  |  Won ' + movie.getString(3) + ' / ' + movie.getString(4) + ' oscars  |  $' + budget + 'm budget  |  ' + movie.getString(9) + '% return' , 20, (barHeight*i)+(barHeight/2+5));
+      titleP.id("title" + i);
+      titleP.parent('myList');
+
+
+      if (movie.getString(2) == "W"){
+        titleP.addClass('bold');
+      }
+
+      //text(movie.getString(1) + "  |  " + movie.getString(0) + " " + won + "  |  Won " + movie.getString(3) + " / " + movie.getString(4) + " oscars  |  $" + budget + "m budget  |  " + movie.getString(9) + "% return" , 20, (barHeight*i)+(barHeight/2+5));     
+    }
+}
+
+function hideAll() {
+  //selectAll('p').hide();
 }
 
 function renderRatings(m, i){
-  fill(0);
   var critic = m.getNum(5)*100;
   var user = m.getNum(6)*100;
   var startingPoint = (windowWidth/100)*user;
   var ratingDiff =  critic - user;
   var circle = false;
 
-  if (ratingDiff < -4 || ratingDiff > 4){
+  if (ratingDiff < -9 || ratingDiff > 9){
     fill(0);
     var circle = true;
   } else {
@@ -79,29 +108,25 @@ function renderRatings(m, i){
     rect(startingPoint, barHeight*i, (windowWidth/100)*ratingDiff, barHeight);
   }
 
-  if (circle == true) {
+  if ((circle == true) && (ratingDiff < 0)) {
     strokeWeight(3);
     stroke(0);
-    fill(241, 246, 25);
+    fill(255,0,0);
+    ellipse(startingPoint,(barHeight*i)+(barHeight/2),12,12);
+  } else if ((circle == true) && (ratingDiff > 0)) {
+    strokeWeight(3);
+    stroke(0);
+    fill(0,255,0);
     ellipse(startingPoint,(barHeight*i)+(barHeight/2),12,12);
   }
+
+  strokeWeight(1);
 }
 
-function draw(){
-  var reps = 4; 
-  var x1 = random(0,windowWidth);
-  var x2 = x1 + random(-2,2);
-  var y1 = random(0,windowHeight);
-  var y2 = y1 + random(-2,2);
-
-  if (frameCount == 10){
-     stroke(0);
-     
-  }
-
-  line(x1,y1,x2,y2); 
-  noStroke();
+function mouseMoved() {
+  var i = Math.round(mouseY/barHeight); 
+  
+  console.log(i);
 }
 
-//get different coords to disappear upon draw
-// apply to entire canvas, not just window
+
